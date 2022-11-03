d291e1ad1d68555ac0d4739dd8a7fee4f284447c
rauschme <98319434+rauschme@users.noreply.github.com>
2022-11-03 10:22:07 +0100

    Update main.py
    IBM cloud functions for get-reviews and post-reviews added

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -30,3 +30,51 @@ def main(dict):
         return {"error": err}
 
     return {"dbs": client.all_dbs()}
+
+# code for get reviews of a specific dealership (identified by ID) used in in IBM cloud:
+import sys 
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+def main(dict): 
+    authenticator = IAMAuthenticator("_kTdHPFbixPEezqxqJNoEZdOPr8QLtSTbohVCkmWDPtH")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("https://c7270dba-8adf-4c66-a968-a977604294cc-bluemix.cloudantnosqldb.appdomain.cloud")
+    response = service.post_find(
+                db='reviews',
+                selector={'dealership': {'$eq': int(dict['id'])}},
+            ).get_result()
+    try: 
+        # result_by_filter=my_database.get_query_result(selector,raw_result=True) 
+        result= {
+            'headers': {'Content-Type':'application/json'}, 
+            'body': {'data':response} 
+            }        
+        return result
+    except:  
+        return { 
+            'statusCode': 404, 
+            'message': 'Something went wrong'
+            }
+    
+# code for post review for a specific dealership used in in IBM cloud:
+import sys
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+def main(dict):
+    authenticator = IAMAuthenticator("_kTdHPFbixPEezqxqJNoEZdOPr8QLtSTbohVCkmWDPtH")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("https://c7270dba-8adf-4c66-a968-a977604294cc-bluemix.cloudantnosqldb.appdomain.cloud")
+    response = service.post_document(db='reviews', document=dict["review"]).get_result()
+    try:
+    # result_by_filter=my_database.get_query_result(selector,raw_result=True)
+        result= {
+        'headers': {'Content-Type':'application/json'},
+        'body': {'data':response}
+        }
+        return result
+    except:
+        return {
+        'statusCode': 404,
+        'message': 'Something went wrong'
+        }
